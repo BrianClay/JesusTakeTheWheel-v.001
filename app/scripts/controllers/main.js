2225214e876231243e894e1bc4af568d2fb4f6b3
Brian Clay <brian.t.clay@gmail.com>
2013-10-15 23:36:29 -0600

    Leg Work
    Very basics are being covered here. Nothing to see.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('JesusTakeTheWheelApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.dattRtfn = [
+            'You have Boilerplate',
+            'Aand AngularJS',
+            'And Karma'
+        ];
+        $scope.gtfo = [
+            'It will be okay',
+            'Or it wont be',
+        ];
+    });
+
+
